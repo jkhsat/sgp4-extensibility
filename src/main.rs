725c7426c11f8305e62e164aab3a0acbc052253a
Jacob <clutch@pop-os.attlocal.net>
2024-07-31 07:26:40 -0500

    laying groundwork for converting to geodetic

--- src/main.rs
+++ src/main.rs
@@ -30,35 +30,52 @@ fn main() -> anyhow::Result<()> {
 
     let elements_vec = parse_3les(&tle_string)?;
 
-    loop {
-        // timestamp to verify speed
-        let poll_timer = time::Instant::now();
-        
-        for element in &elements_vec {
-            let constants = sgp4::Constants::from_elements(element)?;
-            let prediction = constants.propagate(sgp4::MinutesSinceEpoch(( m_sec ) as f64))?;
-            println!("        r = {:?} km", prediction.position);
-            
-            // custom function for converting TEME to ECEF
-            // use this: https://mycoordinates.org/tracking-satellite-footprints-on-earth%E2%80%99s-surface/
-            // satutil::to_ecef(&mut prediction.position)
-        }
-        assert!(poll_timer.elapsed() < sleep_dur);
+    // What is our observer location?
 
-        // Sleep 31ms to simulate 32Hz polling time. 
-        let sleep_check = time::Instant::now();
-        thread::sleep(sleep_dur);
-        assert!(sleep_check.elapsed() >= sleep_dur);
-        
-        // Hey we slept for 32ms... increment 
-        m_sec += 32.0 / 60000.0;
+    // Get satellite position (lets do one satellite for now)
+    let constants = sgp4::Constants::from_elements(&elements_vec[0])?;
+    let prediction = constants.propagate(sgp4::MinutesSinceEpoch(( m_sec ) as f64))?;
 
-        // Stop case timestamp
-        let timestamp = now.elapsed();
-        //println!("        elapsed time: {timestamp:?}");
+    // Get look angle (need obs location for this)
+
+    // Convert sat location to geodetic. Need to do this for distance calculation
+
+
     
-        if timestamp >= one_day {
-            return Ok(())
-        }
-    }
+    return Ok(());
+
+
+    // loop {
+    //     // timestamp to verify speed
+    //     let poll_timer = time::Instant::now();
+        
+    //     for element in &elements_vec {
+    //         let constants: sgp4::Constants = sgp4::Constants::from_elements(element)?;
+    //         let prediction = constants.propagate(sgp4::MinutesSinceEpoch(( m_sec ) as f64))?;
+    //         println!("        r = {:?} km", prediction.position);
+            
+    //         // custom function for converting TEME to ECEF
+    //         // use this: https://mycoordinates.org/tracking-satellite-footprints-on-earth%E2%80%99s-surface/
+    //         // satutil::to_ecef(&mut prediction.position)
+    //     }
+    //     assert!(poll_timer.elapsed() < sleep_dur);
+
+    //     // Sleep 31ms to simulate 32Hz polling time. 
+    //     let sleep_check = time::Instant::now();
+    //     thread::sleep(sleep_dur);
+    //     assert!(sleep_check.elapsed() >= sleep_dur);
+        
+    //     // Hey we slept for 32ms... increment 
+    //     m_sec += 32.0 / 60000.0;
+
+    //     // Stop case timestamp
+    //     let timestamp = now.elapsed();
+    //     //println!("        elapsed time: {timestamp:?}");
+    
+    //     if timestamp >= one_day {
+    //         return Ok(())
+    //     }
+    // }
+
+
 }
--- src/satutil.rs
+++ src/satutil.rs
@@ -1,92 +1,34 @@
-use libm::floor;
-use libm::pow;
+use sgp4::{Error, MinutesSinceEpoch};
 use libm::atan;
 
-const kAE: f64  = 1.0;
-const kQ0: f64  = 120.0;
-const kS0: f64  = 78.0;
-const kMU: f64  = 398600.8;
-const kXKMPER : f64 = 6378.135;
-const kXJ2: f64  = 1.082616e-3;
-const kXJ3: f64  = -2.53881e-6;
-const kXJ4: f64  = -1.65597e-6;
 const kPI: f64 = 3.14159265358979323846264338327950288419716939937510582;
-const kTWOPI: f64 = 2.0 * kPI;
 
-// Need to use non-const values for this... dumb that rust can't do
-// non-const globals. Should I create a separate file for globals?
-pub fn get_kQOMS2T() -> f64 {
-    let non_const_kQ = kQ0;
-    let non_const_kS = kS0;
-    let non_const_kXKMPER = kXKMPER;
-
-    return pow((non_const_kQ - non_const_kS) / non_const_kXKMPER, 4.0);
+pub struct Geodetic { 
+    pub geo_pos: [f64; 3],
 }
 
-pub fn to_ecef(teme_coords: &mut [f64; 3]) { 
-    println!("coords: {:?}", teme_coords);
+pub fn to_geodetic(sat_prediction: sgp4::Prediction, sat_elements: sgp4::Elements) -> core::result::Result<Geodetic, sgp4::Error> { 
 
-    // Calc Greenwich Mean Sidereal Tiem (GMST)
+    // Prediction x,y,z? 
+    let theta = ac_tan(sat_prediction.position[1], sat_prediction.position[0]);
+    let date_time: chrono::naive::NaiveDateTime = sat_elements.datetime;
 
-    // Define earth rotation matrix
+    let lon: f64 = wrap_neg_pos_pi(theta, 
+        to_greenwich_sidereal_time(
+            sat_elements.datetime_to_minutes_since_epoch(&sat_elements.datetime).unwrap().0));
 
-    // Dot product teme and earth rotation matrix
-
-    // profit???
+    Ok(Geodetic {
+        geo_pos: [
+            0.0,
+            0.0,
+            0.0,
+        ],
+    })
 }
 
-// Do this later 
-pub fn geodetic_to_ecef(geodet_coords: [f64; 3]) -> [f64; 3] {
-    let mut ecef_coords: [f64; 3] = [0.0, 0.0, 0.0];
-
-
-    ecef_coords
-}
-
-pub fn check_look_angle(r_lat: f64, r_lon: f64, s_lat: f64, s_lon: f64) -> f64 {
-    let mut look_angle = 0.0;
-
-    look_angle
-}
-
-pub fn to_geodetic(pos_y: f64, pos_x: f64) {
-    // This is going to suck
-}
-
-pub fn Mod(x: f64, y: f64) -> f64 { 
-    if y == 0.0 { 
-        return x;
-    }
-    x - y * floor(x / y)
-}
-
-pub fn wrap_neg_pos_pi(a: f64) -> f64 { 
-    Mod(a + kPI, kTWOPI) - kPI
-}
-
-pub fn wrap_two_pi(a: f64) -> f64 { 
-    Mod(a, kTWOPI)
-}
-
-pub fn wrap_neg_pos_180(a: f64) -> f64 {
-    Mod(a + 180.0, 360.0) - 180.0
-}
-
-pub fn wrap_360(a: f64) -> f64 { 
-    Mod(a, 360.0)
-}
-
-pub fn degrees_to_radians(degrees: f64) -> f64 { 
-    degrees * kPI / 180.0
-}
-
-pub fn radians_to_degrees(radians: f64) -> f64 { 
-    radians * 180.0 / kPI
-}
-
-pub fn ac_tan(sinx: f64, cosx: f64) -> f64 { 
-    if cosx == 0.0 { 
-        if sinx > 0.0 {
+pub fn ac_tan(sin_x: f64, cos_x: f64) -> f64 { 
+    if cos_x == 0.0 { 
+        if sin_x > 0.0 { 
             return kPI / 2.0;
         }
         else { 
@@ -94,11 +36,19 @@ pub fn ac_tan(sinx: f64, cosx: f64) -> f64 {
         }
     }
     else { 
-        if cosx > 0.0 { 
-            return atan(sinx / cosx);
+        if cos_x > 0.0 { 
+            return atan(sin_x / cos_x);
         }
         else { 
-            return kPI + atan(sinx / cosx);
+            return kPI + atan(sin_x / cos_x);
         }
     }
+}
+
+pub fn wrap_neg_pos_pi(theta: f64, greenwich_sr_time: f64) -> f64 { 
+
+}
+
+pub fn to_greenwich_sidereal_time(dt: f64) -> f64 { 
+
 }
