43260072a9b82c6db80920c3f41ce468c8de05d5
Jacob Hicks <jacobhicks@protonmail.com>
2024-08-01 15:30:12 -0500

    add geodetic to ecef function... lay groundwork for ssp and look angle calculations. Maybe I should move things into an impl Observer object

--- src/satutil.rs
+++ src/satutil.rs
@@ -5,12 +5,25 @@ const K_TWO_PI: f64 = K_PI * 2.0;
 const K_F: f64 = 1.0 / 298.26;
 const K_XKMPER: f64 = 6378.135;
 
+// maybe sgp4 has these constants already 
+const SM_AXIS: f64 = 6378.137; // Earth's semi-major axis.
+const E2: f64 = 6.69437999014e-3; // Square of first eccentricity.
+
 pub struct Geodetic { 
-    pub s_lat: f64,
-    pub s_lon: f64, 
-    pub s_alt: f64,
+    pub geo_lat: f64,
+    pub geo_lon: f64,
+    pub geo_alt: f64,
 }
 
+pub struct ECEF {
+    pub ecef_x: f64,
+    pub ecef_y: f64,
+    pub ecef_z: f64,
+}
+
+/// Convert TEME coordinates from sgp4::propagate to geodetic coordinates lat/long/alt
+/// At time of writing this comment I am a rust noob... I am sure there is a better way to write
+/// this function. I will figure that out later. 
 pub fn to_geodetic(sat_prediction: sgp4::Prediction, sat_elements: &sgp4::Elements, t_elapsed: i64) -> core::result::Result<Geodetic, sgp4::Error> { 
 
     // Update a temp epoch with the elapsed time in order to calculate updated sidreal.
@@ -18,21 +31,15 @@ pub fn to_geodetic(sat_prediction: sgp4::Prediction, sat_elements: &sgp4::Elemen
     // Think about refactoring this function now that it works?
     let t_delta: chrono::TimeDelta = chrono::TimeDelta::minutes(t_elapsed);
     let updated_time = &sat_elements.datetime.checked_add_signed(t_delta).unwrap();
-
-    // Prediction x,y,z? degree
     let theta = ac_tan(sat_prediction.position[1], sat_prediction.position[0]);
-
     let r: f64 = ((sat_prediction.position[0] * sat_prediction.position[0]) + (sat_prediction.position[1] * sat_prediction.position[1])).sqrt();
     let e2: f64 = K_F * (2.0 - K_F);
     let mut c;
     let mut phi: f64;
     let mut cnt = 0;
-    
     let mut lon: f64 = wrap_neg_pos_pi(theta - iau_epoch_to_sidereal_time(sgp4::julian_years_since_j2000(updated_time)));
     let mut lat = ac_tan(sat_prediction.position[2], r);
 
-    // phi = geocentric latitude
-    // account for angle between equatorial plane and the point on the surface of the ellipse.
     loop { 
         phi = lat;
         let sinphi: f64 = phi.sin();
@@ -52,16 +59,73 @@ pub fn to_geodetic(sat_prediction: sgp4::Prediction, sat_elements: &sgp4::Elemen
     lon = radians_to_degrees(lon);
 
     let geo = Geodetic {
-        s_lat: lat,
-        s_lon: lon,
-        s_alt: alt,
+        geo_lat: lat,
+        geo_lon: lon,
+        geo_alt: alt,
     };
 
     Ok(geo)
 
 }
 
-pub fn ac_tan(sin_x: f64, cos_x: f64) -> f64 { 
+// Observer?
+/// 15 degree look angle max for sat tx
+pub fn get_look_angle() {
+
+}
+
+// Observer?
+/// Return the Satellite sub-points
+/// I think this is basically just lat/long with altitude at 0... need to double check on celestrak
+pub fn get_ssp() { 
+
+}
+
+// Observer?
+// Also I keep using sgp4::Error... Maybe I should add a few error cases to that enum.
+pub fn get_dist_to_satellite(observer: ECEF, satellite: ECEF) -> core::result::Result<ECEF, sgp4::Error> { 
+    
+    // Placeholders
+    // Stick the 3d distance formula in here when I'm not so lazy.
+    let x = 0.0;
+    let y = 0.0;
+    let z = 0.0;
+
+    let distance = ECEF  {
+        ecef_x: x,
+        ecef_y: y,
+        ecef_z: z,
+    };
+
+    Ok(distance)
+}
+
+/// Takes in geodetic coordinates and converts them to ecef coordinates. 
+/// 
+/// Useful for computing the distance between observer and satellite or observer to SSP.
+/// 
+/// Return a struct of ECEF coordinates.
+pub fn geodetic_to_ecef(geo_latitude: &f64, geo_longitude: &f64, geo_altitude: &f64) -> core::result::Result<ECEF, sgp4::Error> { 
+
+    let lat_rad = degrees_to_radians(geo_latitude);
+    let lon_rad: f64 = degrees_to_radians(geo_longitude);
+    let alt_rad: f64 = degrees_to_radians(geo_altitude);
+
+    let N = SM_AXIS / (1.0 - E2 * (lat_rad*lat_rad).sin());
+    let x = (N + alt_rad) * lat_rad.cos() * lon_rad.cos();
+    let y = (N + alt_rad) * lat_rad.cos() * lon_rad.sin();
+    let z = ((1.0 - E2) * N + alt_rad) * lat_rad.sin();
+
+    let ecef = ECEF { 
+        ecef_x: x,
+        ecef_y: y,
+        ecef_z: z,
+    };
+
+    Ok(ecef)
+}
+
+fn ac_tan(sin_x: f64, cos_x: f64) -> f64 { 
     if cos_x == 0.0 { 
         if sin_x > 0.0 { 
             return K_PI / 2.0;
@@ -80,29 +144,21 @@ pub fn ac_tan(sin_x: f64, cos_x: f64) -> f64 {
     }
 }
 
-pub fn wrap_neg_pos_pi(a: f64) -> f64 { 
+fn wrap_neg_pos_pi(a: f64) -> f64 { 
     return mod_helper(a + K_PI, K_TWO_PI) - K_PI
 }
 
-pub fn mod_helper(x: f64, y: f64) -> f64 { 
+fn mod_helper(x: f64, y: f64) -> f64 { 
     if y == 0.0 { 
         return x;
     }
     return x - y * (x / y).floor();
 }
 
-pub fn radians_to_degrees(radians: f64) -> f64 { 
+fn radians_to_degrees(radians: f64) -> f64 { 
     return radians * 180.0 / K_PI;
 }
 
-// pub fn add_minutes(minutes: f64) -> sgp4::MinutesSinceEpoch { 
-
-// }
-
-// pub fn add_microseconds(microseconds: f64) -> sgp4::MinutesSinceEpoch { 
-
-// }
-
-// pub fn add_ticks(ticks: u64) -> sgp4::MinutesSinceEpoch { 
-
-// }
+fn degrees_to_radians(degrees: &f64) -> f64 { 
+    return degrees * K_PI / 180.0
+}
